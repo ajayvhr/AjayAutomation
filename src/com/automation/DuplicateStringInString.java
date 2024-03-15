cc8f7ea21febc29a780bffe754ddade6704db8f6
ajayvhr <ajayvhrshorts@gmail.com>
2024-03-15 04:20:55 -0700

    commit

--- src/com/automation/DuplicateStringInString.java
+++ src/com/automation/DuplicateStringInString.java
@@ -1,12 +1,48 @@
 package com.automation;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 public class DuplicateStringInString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("AJAY");
+		getDuplicate( "My name is AJAY KUMAR");
 
 	}
+	
+	public static void getDuplicate(String str) {
+		
+		char[] strArray=str.toCharArray();
+		
+		Map<Character,Integer> map = new HashMap<Character,Integer>();
+				
+			for(char ch : strArray) {
+				if(map.containsKey(ch)) {
+					map.put(ch, map.get(ch)+1);
+				}
+				else
+					map.put(ch,1);
+				
+			}
+			
+			Set<Map.Entry<Character,Integer>> entrySet =map.entrySet();
+			
+			for(Map.Entry<Character,Integer> entry : entrySet) {
+				
+				if(entry.getValue()>1) {
+					System.out.println(entry.getKey() + ":" + entry.getValue());
+				}
+				
+				
+			}
+			
+		}
+		
+		
+		
+	}
 
-}
